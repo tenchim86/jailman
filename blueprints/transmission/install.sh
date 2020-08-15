414a66d9deb6b164afa8731a0dbaee9d412c5a72
Tenchi <69703290+tenchim86@users.noreply.github.com>
2020-08-14 22:22:48 -0400

    Update install.sh

--- blueprints/transmission/install.sh
+++ blueprints/transmission/install.sh
@@ -3,17 +3,17 @@
 
 # Check if dataset Downloads dataset exist, create if they do not.
 # shellcheck disable=SC2154
-createmount "$1" "${global_dataset_downloads}" /mnt/downloads
+createmount "$1" "${global_dataset_downloads}" /mnt/Downloads
 
 # Check if dataset Complete Downloads dataset exist, create if they do not.
-createmount "$1" "${global_dataset_downloads}"/complete /mnt/downloads/complete
+createmount "$1" "${global_dataset_downloads}"/Complete /mnt/Downloads/Complete
 
 # Check if dataset InComplete Downloads dataset exist, create if they do not.
-createmount "$1" "${global_dataset_downloads}"/incomplete /mnt/downloads/incomplete
+createmount "$1" "${global_dataset_downloads}"/Incomplete /mnt/Downloads/Incomplete
 
 
 iocage exec "$1" chown -R transmission:transmission /config
 iocage exec "$1" sysrc "transmission_enable=YES"
 iocage exec "$1" sysrc "transmission_conf_dir=/config"
-iocage exec "$1" sysrc "transmission_download_dir=/mnt/downloads/complete"
+iocage exec "$1" sysrc "transmission_download_dir=/mnt/Downloads/Complete"
 iocage exec "$1" service transmission restart

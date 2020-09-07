b1a50abfc3cd44bc450d259262d4b711ec5ebefe
Tenchi <69703290+tenchim86@users.noreply.github.com>
2020-09-07 15:08:25 -0400

    Update update.sh

--- blueprints/sabnzbd/update.sh
+++ blueprints/sabnzbd/update.sh
@@ -1,10 +1,10 @@
 #!/usr/local/bin/bash
 # This file contains the update script for sonarr
 
-iocage exec "$1" service sonarr stop
-#TODO insert code to update sonarr itself here
-iocage exec "$1" chown -R sonarr:sonarr /usr/local/share/NzbDrone /config
+iocage exec "$1" service sabnzbd stop
+#TODO insert code to update sabnzbd itself here
+iocage exec "$1" chown -R sabnzbd:sabnzbd /usr/local/share/NzbDrone /config
 # shellcheck disable=SC2154
-cp "${SCRIPT_DIR}"/blueprints/sonarr/includes/sonarr.rc /mnt/"${global_dataset_iocage}"/jails/"$1"/root/usr/local/etc/rc.d/sonarr
-iocage exec "$1" chmod u+x /usr/local/etc/rc.d/sonarr
-iocage exec "$1" service sonarr restart
+cp "${SCRIPT_DIR}"/blueprints/sabnzbd/includes/sabnzbd.rc /mnt/"${global_dataset_iocage}"/jails/"$1"/root/usr/local/etc/rc.d/sabnzbd
+iocage exec "$1" chmod u+x /usr/local/etc/rc.d/sabnzbd
+iocage exec "$1" service sabnzbd restart

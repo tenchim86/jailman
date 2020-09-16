2c1ea9c547861892b7d21ab590723ef7213cc340
Tenchi <69703290+tenchim86@users.noreply.github.com>
2020-09-15 21:59:20 -0400

    Update install.sh

--- blueprints/sabnzbd/install.sh
+++ blueprints/sabnzbd/install.sh
@@ -11,9 +11,15 @@ createmount "$1" "${global_dataset_downloads}"/Complete /mnt/Downloads/Complete
 # Check if dataset InComplete Downloads dataset exist, create if they do not.
 createmount "$1" "${global_dataset_downloads}"/Incomplete /mnt/Downloads/Incomplete
 
-
 iocage exec "$1" chown -R sabnzbd:sabnzbd /config
 iocage exec "$1" sysrc "sabnzbdn_enable=YES"
 iocage exec "$1" sysrc "sabnzbd_conf_dir=/config"
-iocage exec "$1" sysrc "sabnzbd_download_dir=/mnt/Downloads/Complete"
 iocage exec "$1" service sabnzbd restart
+
+# Force update pkg to get latest sabnzbd version
+iocage exec "$1" pkg update
+iocage exec "$1" pkg install sabnzbdplus
+iocage exec "$1" sysrc sabnzbd_enable="YES"
+iocage exec "$1" service sabnzbd start
+
+echo "Finished installing sabnzbd"
